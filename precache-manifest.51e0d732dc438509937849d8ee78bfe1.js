0314acb67269550b6fa8c972a1b643b5b164f5f8
andreyehin <syrosework@gmail.com>
2019-01-28 22:42:21 +0300

    Updates

---
+++ precache-manifest.51e0d732dc438509937849d8ee78bfe1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2926dc46efaabfb624e67b0db270c8a",
+    "url": "/mytodo-list/static/media/mark.f2926dc4.png"
+  },
+  {
+    "revision": "17ef216d195b833ec36a1b05bb6f27d6",
+    "url": "/mytodo-list/static/media/mark-unchecked.17ef216d.png"
+  },
+  {
+    "revision": "4ff40143d5d782a29fe8ed46d83db89a",
+    "url": "/mytodo-list/static/media/delete.4ff40143.png"
+  },
+  {
+    "revision": "82508ed4ba0347098b22",
+    "url": "/mytodo-list/static/js/runtime~main.82508ed4.js"
+  },
+  {
+    "revision": "3749b22a782eb3f81ecf",
+    "url": "/mytodo-list/static/js/main.3749b22a.chunk.js"
+  },
+  {
+    "revision": "08102e512a7577f79a22",
+    "url": "/mytodo-list/static/js/1.08102e51.chunk.js"
+  },
+  {
+    "revision": "3749b22a782eb3f81ecf",
+    "url": "/mytodo-list/static/css/main.10a117a1.chunk.css"
+  },
+  {
+    "revision": "af454929300b89eaa725a34a756e645b",
+    "url": "/mytodo-list/index.html"
+  }
+];
